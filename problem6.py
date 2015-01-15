d56b5fd8fa0d41b2c743c9a894c495ae8fdc6bdc
John Stafford <john@jkms.me>
2015-01-14 16:11:55 -0800

    Problem 6

---
+++ problem6.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import math
+
+sums = 0
+for x in range(1,101):
+	sums += math.pow(x,2)
+
+print sums
+
+
+sumsx = 0
+for x in range(1,101):
+	sumsx += x
+
+sumsx = math.pow(sumsx,2)	
+print sumsx
+
+answer = sumsx-sums
+
+print answer
