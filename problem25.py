5459e31231d4b95fe740265181f9dcb4d350d529
John Stafford <john@jkms.me>
2015-01-24 21:58:58 -0800

    problem 25

---
+++ problem25.py
@@ -0,0 +1,32 @@
+#  problem25.py
+#  
+#  Copyright 2015 John Stafford <john@jkms.me>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+#!/usr/bin/env python
+seq=[1,1]
+def fib_seq():
+	seq.append(seq[-1]+seq[-2])
+	return seq
+
+fib_len=0
+while fib_len <= 999:
+	fib_seq()
+	fib_len = len(str(seq[-1]))
+	
+print "the answer is", len(seq)
